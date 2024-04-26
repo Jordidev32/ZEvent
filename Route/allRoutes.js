1a28d78a70878728a30a93654ea99ab4d236ae31
Guyot <jordanguyot32@gmail.com>
2024-04-26 12:09:51 +0200

    Carousel ajouté

--- Route/allRoutes.js
+++ Route/allRoutes.js
@@ -2,7 +2,8 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/planning", "Planning", "/pages/planning.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zevent";
